4e1519f9c69139104c5c9d785716fdeaa4338ebc
Ramiro Tabolini <ramiro.tabolini@mi.unc.edu.ar>
2024-09-27 21:29:35 -0300

    add function printMatrix

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <optional>
+#include <algorithm>
+#include <sstream>
+#include <iterator>
 
+using namespace std;
 
 using Matrix = vector<vector<optional<int>>>;
 
-//Funcion para 
-
+//Funcion para mostrar la matrix
+void printMatrix(const Matrix& matrix) {
+    // Iteramos sobre cada fila
+    std::for_each(matrix.begin(), matrix.end(), [](const vector<optional<int>>& row) {
+        // Usamos std::transform para formatear cada celda y guardarlo en un string
+        std::ostringstream oss;
+        std::transform(row.begin(), row.end(), std::ostream_iterator<string>(oss, " "), [](const optional<int>& cell) {
+            // Formateamos cada celda: si es nullopt, ponemos ".", si no, mostramos el número
+            return cell ? to_string(*cell) : ".";
+        });
+        cout << oss.str() << endl;  // Imprimimos la fila formateada
+    });
+}
 
 // Funcion para crear un grid inicial
 Matrix createInitialMatrix(){
@@ -25,7 +40,8 @@ Matrix createInitialMatrix(){
 
 int main(){
     std::cout << "Hola" << std::endl;
-    
+    Matrix matrix = createInitialMatrix();
+    printMatrix(matrix);
 
     return 0;
 }
